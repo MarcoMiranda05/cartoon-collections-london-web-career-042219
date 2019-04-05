b5c65b7e51069fbcda02862ca72e4d29eb08c6fd
Marco Miranda <marmirand@hotmail.com>
2019-04-05 12:28:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-def roll_call_dwarves
-  dwarves = []
-  dwarves.each do |dwarf|
-    puts "#{dwarf}"
+def roll_call_dwarves(dwarves)
+
+   dwarves.each_with_index do | dwarf, index |
+    puts "#{index+1} #{dwarf}"
   end
 end
 
